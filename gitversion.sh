f864e176c2bec9379316b9b542bb80f40164adcd
yb <>
2024-04-21 15:08:45 +0700

    update code

--- gitversion.sh
+++ gitversion.sh
@@ -14,11 +14,11 @@ build_number=$(echo $latest_tag | cut -d '-' -f 2)
 latest_commit_message=$(git log -1 --pretty=%B)
 
 # 检查提交消息中是否包含特定关键词并执行相应的操作
-if [[ "$latest_commit_message" == *"trigger_build1"* ]]; then
+if [[ "$latest_commit_message" == *"SIT"* ]]; then
     echo "Building from commit: $latest_commit_message"
     # 这里替换为触发构建1的实际命令
     # build_command1
-elif [[ "$latest_commit_message" == *"trigger_build2"* ]]; then
+elif [[ "$latest_commit_message" == *"UAT"* ]]; then
     echo "Building from commit: $latest_commit_message"
     # 这里替换为触发构建2的实际命令
     # build_command2
